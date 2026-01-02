9d78e4c712a6e6ec1fd04418b6bf506d26679499
Omrawat11 <omrawat2215@gmail.com>
2026-01-02 19:56:29 +0530

    first commit

---
+++ two_sum.py
@@ -0,0 +1,17 @@
+        # Q- 2206- divide array into equal pairs
+# l = [3,2,3,2,2,2]
+# d = {}
+# for i in l:
+#     if i not in d:
+#         d[i] = 1
+#     else:
+#         d[i] +=1
+# for j in d.values():
+#     if j % 2 !=0:
+#         print("False")
+# retun True
+
+
+
+
+         
