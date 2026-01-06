819bd862a0555a214e4830dc22beb7dc1df4ed6a
Om Rawat <omrawat2215@gmail.com>
2026-01-06 19:24:59 +0530

    Add files via upload

---
+++ 2210_ Count_Hills_and_Valleys_in_an_Array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countHillValley(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        prev = nums[0]
+        for i in range(1,len(nums)-1):
+            if nums[i] == nums[i+1]:
+                continue
+            if (nums[i] > prev and nums[i] > nums[i+1]) or \
+               (nums[i] < prev and nums[i] < nums[i+1]):
+                  count +=1
+            prev = nums[i]
+        return count
