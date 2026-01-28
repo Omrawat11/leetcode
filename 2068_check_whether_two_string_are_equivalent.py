df7408d83354569e27152588ecb0a8ae4f1af09d
Om Rawat <omrawat2215@gmail.com>
2026-01-28 21:15:44 +0530

    Add files via upload

---
+++ 2068_check_whether_two_string_are_equivalent.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def checkAlmostEquivalent(self, word1, word2):
+        """:type word1: str
+        :type word2: str
+        :rtype: bool
+        """
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+
+        for ch in word1:
+            freq1[ord(ch) - ord('a')] += 1
+
+        for ch in word2:
+            freq2[ord(ch) - ord('a')] += 1
+
+        for i in range(26):
+            if abs(freq1[i] - freq2[i]) > 3:
+                return False
+
+        return True
